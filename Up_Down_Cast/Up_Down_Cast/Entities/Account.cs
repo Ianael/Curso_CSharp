222177c72afc3c9e1d3c4207950eb99630c30f3b
Ianael <ianael2409@hotmail.com>
2021-06-07 09:03:46 -0300

    Add files via upload

---
+++ Up_Down_Cast/Up_Down_Cast/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Up_Down_Cast.Entities {
+    class Account {
+        public int Number { get; private set; }
+        public string Holder { get; private set; } //private -> acesso, mas não alteração
+        public double Balance { get; protected set; } //protected -> acesso e alteração(por subclasses)
+
+        public Account() { }
+        public Account(int number, string holder, double balance) {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount) {
+            Balance += amount;
+        }
+    }
+}
---
+++ Up_Down_Cast/Up_Down_Cast/Entities/SavingsAccount.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Up_Down_Cast.Entities {
+    class SavingsAccount : Account{
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance) {
+            InterestRate = interestRate;
+        }
+
+        public void updateBalance() {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
