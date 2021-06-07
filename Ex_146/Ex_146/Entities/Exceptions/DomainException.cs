18c7d3182481d01ae6ce5effe9c93452ce785b7f
Ianael <ianael2409@hotmail.com>
2021-06-07 15:44:44 -0300

    Add files via upload

---
+++ Ex_146/Ex_146/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ex_146.Entities.Exceptions {
+    class DomainException : ApplicationException{
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
---
+++ Ex_146/Ex_146/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Ex_146.Entities;
+using Ex_146.Entities.Exceptions;
+
+namespace Ex_146 {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Console.WriteLine("Enter account data: ");
+
+                Console.Write("Number: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double iniBal = int.Parse(Console.ReadLine());
+
+                Console.Write("Withdraw limit: ");
+                double withLim = int.Parse(Console.ReadLine());
+
+                Account acc = new Account(num, name, iniBal, withLim);
+
+                Console.Write("Enter the amount to withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                
+                acc.Withdraw(withdraw);
+
+                Console.WriteLine("New Balance: " + acc.Balance.ToString());
+            } catch(DomainException e){
+                Console.WriteLine("Withdraw Error: " + e.Message);
+            }catch (Exception e) {
+                Console.WriteLine("Unexpected ERROR: " + e.Message);
+            }
+        } 
+    }
+}
