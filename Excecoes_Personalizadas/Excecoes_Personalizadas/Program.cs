6913e2c84e34b328729cf3db7e9ee8ff73a3aae3
Ianael <ianael2409@hotmail.com>
2021-06-07 13:46:40 -0300

    Add files via upload

---
+++ Excecoes_Personalizadas/Excecoes_Personalizadas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Excecoes_Personalizadas.Entities;
+using Excecoes_Personalizadas.Entities.Exceptions;
+
+namespace Excecoes_Personalizadas {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Console.Write("Room number: ");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime ckin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime ckout = DateTime.Parse(Console.ReadLine());
+
+
+                Reservation res = new Reservation(num, ckin, ckout);
+                Console.WriteLine("Reservation: " + res);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                ckin = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                ckout = DateTime.Parse(Console.ReadLine());
+
+                res.UpdateDates(ckin, ckout);
+                Console.WriteLine("Reservation: " + res);
+            } catch (DomainException e) {
+                Console.WriteLine("Error in Reservation: " + e.Message);
+            } catch (FormatException e) {
+                Console.WriteLine("Format error: " + e.Message);
+            } catch (Exception e) {
+                Console.WriteLine("Enexpected Error: " + e.Message);
+            }
+        }
+    }
+}
+
