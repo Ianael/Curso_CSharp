ac727f712349e6a2004910c5e2b1b99e3b04ae1f
Ianael <ianael2409@hotmail.com>
2021-06-07 08:32:44 -0300

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex1 {
+    class Program {
+        static void Main(string[] args) {
+            string produto1 = "Computador", produto2 = "Mesa de Escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{produto1}, cujo preço é ${preco1}");
+            Console.WriteLine($"{produto2}, cujo preço é ${preco2}");
+            Console.WriteLine();
+            Console.WriteLine("Registro: 30 anos de idade, código {0} e gênero: {1}", codigo, genero);
+            Console.WriteLine();
+            Console.WriteLine($"Medida com oito casas decimais: {medida:f8}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:f3}");
+            Console.WriteLine($"Separador decimal invariant culture: " + medida.ToString("f3", System.Globalization.CultureInfo.InvariantCulture));
+
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------");
+
+            Console.Write("Escreva 3 palavras: ");
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine("Você digitou: {0}, {1} e {2}", s[0], s[1], s[2]);
+
+        }
+    }
+}
---
+++ Ex_Listas/Ex_Listas/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic; //Listas
+
+namespace Ex_Listas {
+
+    class Program {
+        static bool Test(string s) {
+            return s[0] == 'A';
+        }
+
+        static void Main(string[] args) {
+            List<string> l = new List<string> {"Mary", "Steve", "Robert"};
+            List<string> list = new List<string>();
+
+            list.Add("Black");
+            list.Add("Anne");
+            list.Add("Bob");
+            list.Add("Sanders");
+            list.Add("Andrew");
+
+            list.Insert(2, "Mark");
+            list.Insert(3, "Sandy");
+
+            foreach (string x in list) {
+                Console.WriteLine(x);   
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("O tamanho da lista é: " + list.Count);
+            Console.WriteLine("Primeiro elemento com A: " + list.Find(x => x[0] == 'A')); //list.Find(Test)
+            Console.WriteLine("Último elemento com A: " + list.FindLast(x => x[0] == 'A'));
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Primeira posição com A: " + list.FindIndex(x => x[0] == 'A'));
+            Console.WriteLine("Última posição com A: " + list.FindLastIndex(x => x[0] == 'A'));
+            Console.WriteLine(" ");
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("Nomes com 5 characters: ");
+            foreach (string x in list2) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine(" ");
+            list.Remove("Bob");
+            foreach (string x in list) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine(" ");
+            list.RemoveAll(x => x[0] == 'S');
+            foreach (string x in list) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine(" ");
+            list.RemoveAt(1);
+            foreach (string x in list) {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine(" ");
+            list.RemoveRange(0, 2); //(inicio, qnt)
+            foreach (string x in list) {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
