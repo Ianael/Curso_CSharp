5b80b4a92a69e3130781bfdee2bded705b2c112a
Ianael <ianael2409@hotmail.com>
2021-06-07 08:15:46 -0300

    Add files via upload

---
+++ Primeiro/Primeiro/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Primeiro {
+    class Program {
+        static void Main(string[] args) {
+            bool b = true;
+            char beta = '\u03b2'; //unicode
+
+            object obj = "Black"; //tipo genérico
+
+            Console.WriteLine(obj);
+
+            obj = 2.5f;
+
+            Console.WriteLine(obj);
+
+            //Min/Max -> tipo.MinValue ou tipo.MaxValue
+            int n1 = int.MaxValue;
+            Console.WriteLine(n1);
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            double saldo = 12.567448;
+            string s;
+
+            s = saldo.ToString("F4"); //FN p número de casas decimais
+
+            Console.WriteLine(s);
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            int idade = 32;
+            string nome = "João";
+            Console.WriteLine("{0} tem {1} anos e saldo igual a {2:F2} reais.", nome, idade, saldo);
+            Console.WriteLine($"{nome} tem {idade} anos e saldo de {saldo:f2} reais.");
+            Console.WriteLine(nome+ " tem " + idade + " anos.");
+        }
+    }
+}
+
---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Strings {
+    class Program {
+        static void Main(string[] args) {
+
+            string original = "Olá, sou a frase ORIGINAL! ";
+            Console.WriteLine("Original: - " + original + "-");
+
+            string s1 = original.ToUpper();
+            Console.WriteLine("ToUpper: - " + s1 + "-");
+
+            string s2 = original.ToLower();
+            Console.WriteLine("ToLower: - " + s2 + "-");
+
+            string s3 = original.Trim();
+            Console.WriteLine("Trim: - " + s3 + "-");
+
+            int n1 = original.IndexOf("a");
+            Console.WriteLine("IndexOf('a'): " + n1);
+
+            int n2 = original.LastIndexOf("a");
+            Console.WriteLine("LastIndexOf('a'): " + n2);
+
+            string s4 = original.Substring(4);
+            Console.WriteLine("Substring(4): " + s4);
+
+            string s5 = original.Substring(4, 6);
+            Console.WriteLine("Substring(4, 6): " + s5);
+
+            string s6 = original.Replace('a', 'e');
+            Console.WriteLine("Replace('a', 'e'): " + s6);
+
+            string s7 = original.Replace("ORIGINAL", "IMPOSTORA");
+            Console.WriteLine("Replace('ORIGINAL', 'IMPOSTORA'): " + s7);
+
+            bool b1 = string.IsNullOrEmpty(original);
+            Console.WriteLine("IsNullOrEmpty: " + b1); //""
+
+            bool b2 = string.IsNullOrWhiteSpace(original);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2); //"   "
+        }
+    }
+}
