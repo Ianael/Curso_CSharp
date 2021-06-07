4a1c9de8a8ced1c209512804d06c074a33ee0564
Ianael <ianael2409@hotmail.com>
2021-06-07 10:41:17 -0300

    Add files via upload

---
+++ Ex_133/Ex_133/Entities/ImportedProduct.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ex_133.Entities {
+    class ImportedProduct : Product{
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) : base(name, price){
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice() {
+            return Price+CustomsFee;
+        }
+
+        public override string PriceTag() {
+            return Name + " $" + TotalPrice().ToString() + " (Custom fee: $" + CustomsFee + ")";
+        }
+    }
+}
