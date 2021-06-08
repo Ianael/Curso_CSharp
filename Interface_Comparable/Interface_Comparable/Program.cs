4ddc4fa393f759127dd5eb1a0285b588d0517bd9
Ianael <ianael2409@hotmail.com>
2021-06-08 16:59:16 -0300

    Add files via upload

---
+++ Interface_Comparable/Interface_Comparable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Interface_Comparable.Entities;
+
+namespace Interface_Comparable {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\ianael\Documents\curso_csharp\Interface_Comparable\funcionarios.txt";
+
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    List<Employee> list = new List<Employee>();
+
+                    while (!sr.EndOfStream) {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+
+                    foreach (Employee emp in list) {
+                        Console.WriteLine(emp);
+                    }
+                }
+
+
+            } catch (Exception e) {
+                Console.WriteLine("ERROR: " + e.Message);
+            }
+        }
+    }
+}
