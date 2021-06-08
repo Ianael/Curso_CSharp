19aaaacf9c45ecc151d7c7394105c7506f18b24c
Ianael <ianael2409@hotmail.com>
2021-06-08 17:15:23 -0300

    Add files via upload

---
+++ Tipo_Generics/Tipo_Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tipo_Generics {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                PrintService printService = new PrintService();
+
+                Console.Write("How many values? ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                for (int i=0; i< n; i++) {
+                    Console.Write($"Value #{i}: ");
+                    int x = int.Parse(Console.ReadLine());
+                    printService.AddValue(x);
+                }
+
+                Console.WriteLine();
+                printService.Print();
+                Console.WriteLine("First: " + printService.First());
+
+            } catch(Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
