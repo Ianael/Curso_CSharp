c60b816d092efd6bd87a22b6143c501703f3f96e
Ianael <ianael2409@hotmail.com>
2021-06-09 09:33:42 -0300

    Add files via upload

---
+++ Ex_211/Ex_211/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Ex_211.Entities;
+
+namespace Ex_211 {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\ianael\Documents\curso_csharp\Ex_211\in.txt";
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+            try {
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+
+            } catch (Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
