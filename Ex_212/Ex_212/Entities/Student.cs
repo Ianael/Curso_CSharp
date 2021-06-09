3a04b6f903d8be4871644606e0bc7767930b112f
Ianael <ianael2409@hotmail.com>
2021-06-09 09:32:35 -0300

    Add files via upload

---
+++ Ex_212/Ex_212/Entities/Student.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Ex_212.Entities {
+    class Student {
+        public int Code { get; set; }
+
+        public Student(int code) {
+            Code = code;
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Student)) {
+                return false;
+            }
+            Student other = obj as Student;
+            return Code.Equals(other.Code);
+        }
+
+        public override int GetHashCode() {
+            return Code.GetHashCode();
+        }
+    }
+}
---
+++ Ex_212/Ex_212/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Ex_212.Entities;
+
+namespace Ex_212 {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<Student> students = new HashSet<Student>();
+            try {
+                Console.Write("How many student in course A? ");
+                int na = int.Parse(Console.ReadLine());
+
+                for (int i=1; i <= na; i++) {
+                    Console.Write($"#{i} code in A: ");
+                    int s = int.Parse(Console.ReadLine());
+                    students.Add(new Student(s));
+                }
+
+                Console.WriteLine();
+                Console.Write("How many student in course B? ");
+                int nb = int.Parse(Console.ReadLine());
+
+                for (int i=1; i <= nb; i++) {
+                    Console.Write($"#{i} code in B: ");
+                    int s = int.Parse(Console.ReadLine());
+                    students.Add(new Student(s));
+                }
+
+                Console.WriteLine();
+                Console.Write("How many student in course C? ");
+                int nc = int.Parse(Console.ReadLine());
+
+                for (int i=1; i <= nc; i++) {
+                    Console.Write($"#{i} code in C: ");
+                    int s = int.Parse(Console.ReadLine());
+                    students.Add(new Student(s));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Total students: " + students.Count);
+            } catch(Exception e) {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
