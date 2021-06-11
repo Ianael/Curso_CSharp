76cf5204d86441e58fbaf5cad4aed702f67569ff
Ianael <ianael2409@hotmail.com>
2021-06-11 08:53:36 -0300

    Add files via upload

---
+++ Comparison/Comparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Comparison.Entities;
+
+namespace Comparison {
+    class Program {
+        static void Main(string[] args) {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            //Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper())); //Função Lambda
+
+            foreach(Product p in list) {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Delegates.Services;
+
+namespace Delegates {
+    class Program {
+
+        //delegate double BinaryNumericOperation(double n1, double n2);
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args) {
+            double a = 10;
+            double b = 12;
+            
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            //double result = CalculationService.Max(a, b); //Maior
+            //double result = op(a, b);
+
+            op.Invoke(a, b); //Invoke executa todos os metodos na ordem que foram adicionados (utilizar metodos void)
+            op(a, b);
+
+            //Console.WriteLine(result);
+        }
+    }
+}
