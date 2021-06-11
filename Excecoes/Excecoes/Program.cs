9afd2123d657d97b125f50996ce9cc6abc187d51
Ianael <ianael2409@hotmail.com>
2021-06-11 08:52:47 -0300

    Add files via upload

---
+++ Excecoes/Excecoes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Excecoes {
+    class Program {
+        static void Main(string[] args) {
+            FileStream fs = null;
+
+            /*int result = 0;
+            try {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                result = n1 / n2;
+            } catch (DivideByZeroException e) {
+                Console.WriteLine("ERROR! " + e.Message);
+            } catch (FormatException) {
+                Console.WriteLine("Erro de tipagem! ");
+            }
+
+            Console.WriteLine("Resultado: " + result.ToString());*/
+
+            //Exemplo com arquivo
+            try {
+                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            } finally {
+                if (fs != null) {
+                    fs.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Files/Files/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Files {
+    class Program {
+        static void Main(string[] args) {
+            string path = @"C:\Users\ianael\Documents\curso_csharp\Files\File.txt";
+            string targetPath = @"C:\Users\ianael\Documents\curso_csharp\Files\Out.txt";
+            //StreamReader sr = null;
+            try {
+                /*FileInfo fs = new FileInfo(sourcePath);
+                fs.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach (string s in lines) {
+                    Console.WriteLine(s);
+                }*/
+
+                /*sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }*/
+
+                //Fecha arquivo automaticamente
+                /* using (FileStream fs = new FileStream(path, FileMode.Open)) {
+                     using (StreamReader sr = new StreamReader(fs)) {
+                         while (!sr.EndOfStream) {
+                             string line = sr.ReadLine();
+                             Console.WriteLine(line);
+                         }
+                     }
+                 }*/
+
+                /*using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }*/
+
+                string[] lines = File.ReadAllLines(path);
+
+                using (StreamWriter sw = File.AppendText(targetPath)) { //append abre e escreve
+                    foreach (string line in lines) {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+
+            }catch(IOException e) {
+                Console.WriteLine("ERROR: "+ e.Message);
+            } /*finally {
+                if (sr != null) {
+                    sr.Close();
+                }
+                //if (fs != null) fs.Close();
+               
+            }*/
+        }
+    }
+}
