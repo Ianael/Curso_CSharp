473c44afcd7ca9596c3b2c4b0af22778aef73806
Ianael <ianael2409@hotmail.com>
2021-06-11 08:55:09 -0300

    Add files via upload

---
+++ Metodos_Extensao/Metodos_Extensao/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Metodos_Extensao {
+    class Program {
+        static void Main(string[] args) {
+            DateTime dt = new DateTime(2021, 6, 9, 8, 10, 5);
+            Console.WriteLine(dt.ElapsedTime());
+
+            String s = "Good morning dear students!";
+            Console.WriteLine(s.Cut(10));
+        }
+    }
+}
