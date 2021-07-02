131b5816458ccf0aa637dda2c204fec369d1ad74
Ianael <ianael2409@hotmail.com>
2021-07-02 17:04:22 -0300

    Add files via upload

---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Recursividade {
+    class Program {
+        static int Fatorial(int num) {
+            if (num <= 0)
+                return 1;
+            
+            return num * Fatorial(num-1);
+        }
+        
+        static void Main(string[] args) {
+            Console.Write("Deseja saber o fatorial de qual número? ");
+            int num = int.Parse(Console.ReadLine());
+            
+            int result = Fatorial(num);
+
+            Console.WriteLine();
+            Console.WriteLine($"O fatorial de {num} é {result}");
+        }
+    }
+}
