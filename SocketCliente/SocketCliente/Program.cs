66d9a961c2da613e18a75e26942f167be5364940
Ianael <ianael2409@hotmail.com>
2021-07-13 16:52:55 -0300

    Add files via upload

---
+++ SocketCliente/SocketCliente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketCliente {
+    class Program {
+        static void Main(string[] args) {
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //IPV4, stream, confirmação
+
+            try {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234); //10.7.2.8
+
+                socket.Connect(endPoint);
+
+                Console.WriteLine("Conectado com sucesso!");
+                Console.WriteLine();
+                Console.Write("Insira a informação a ser enviada: ");
+                string info = Console.ReadLine();
+                Console.WriteLine();
+
+                byte[] buffer = Encoding.Default.GetBytes(info);
+                socket.Send(buffer, 0, buffer.Length, SocketFlags.None);
+
+                Console.WriteLine("Mensagem enviada!");
+                Console.WriteLine();                
+            } catch (Exception ex) {
+                Console.WriteLine("Não foi possível conectar ao servidor! " + ex.Message);
+            }
+
+            socket.Close();
+
+            Console.Write("Pressione uma tecla para fechar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SocketServer/SocketServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer {
+    class Program {
+        static void Main(string[] args) {
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); //IPV4, stream, confirmação
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234); //10.7.2.8
+
+            socket.Bind(endPoint);
+
+            socket.Listen(5);
+
+            Console.WriteLine("Escutando...");
+            Console.WriteLine();
+
+            Socket escutando = socket.Accept();
+
+            byte[] buffer = new byte[255];
+
+            int tamanho = escutando.Receive(buffer, 0, buffer.Length, SocketFlags.None);
+
+            Array.Resize(ref buffer, tamanho);
+
+            Console.Write("Recebido: ");
+            Console.WriteLine(Encoding.Default.GetString(buffer));
+
+            socket.Close();
+
+            Console.WriteLine();
+            Console.Write("Comunicação finalizada! \nPressione qualquer tecla para fechar...");
+            Console.ReadKey();
+        }
+    }
+}
